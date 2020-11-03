04862836e449c00cdad9f73e4f4256265bedc85b
Taishi <taishi.k0903@gmail.com>
2020-11-02 18:16:09 -0800

    Update mac-apps.sh

--- .bin/mac-apps.sh
+++ .bin/mac-apps.sh
@@ -7,7 +7,7 @@ function existsApp () {
   brew list --cask $1 > /dev/null 2>&1
 }
 
-APPS=('google-chrome' 'firefox' 'iterm2' 'postman' 'spotify' 'visual-studio-code' 'toggl-track' 'slack' 'keepingyouawake' 'clipy' 'fontforge' 'google-japanese-ime')
+APPS=('google-chrome' 'firefox' 'iterm2' 'postman' 'spotify' 'visual-studio-code' 'toggl-track' 'slack' 'keepingyouawake' 'clipy' 'fontforge' 'google-japanese-ime' 'alfred')
 
 function installApps () {
   command printf "${ESC}[36m%s${ESC}[m\n" 'Installing Mac Apps'
