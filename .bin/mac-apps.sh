057c206bbcacc10f5a20771006f73964d42be9c5
taishikato <taishi.k0903@gmail.com>
2021-02-03 12:15:42 -0800

    Revert "feat: background process"
    This reverts commit 52c0535ec1fe0c5abbca57163e44287870b730ad.

--- .bin/mac-apps.sh
+++ .bin/mac-apps.sh
@@ -17,13 +17,9 @@ function installApps () {
     then
       command printf "${ESC}[${GREEN}%s${ESC}[m\n" "[info][$v] does not exist"
       command printf "${ESC}[${GREEN}%s${ESC}[m\n" "[info][$v] installing it…"
-      command brew install --cask $v &
+      command brew install --cask $v
     else
       command printf "${ESC}[${GREEN}%s${ESC}[m\n" "[info][$v] already exists"
     fi
   done
-
-  command wait
-
-  command echo "Info: done"
 }
